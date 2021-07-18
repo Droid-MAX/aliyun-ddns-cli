65a80db4d3f945db14c87e2dff54472c345c8ca2
Droid-MAX <droidmax61@gmail.com>
2021-07-18 15:52:25 +0800

    Add new IPv4 & IPv6 Api and Update build-release.sh

--- build-release.sh
+++ build-release.sh
@@ -7,12 +7,7 @@ if [[ "$(uname)" == 'Darwin' ]]; then
 	MD5='md5'
 fi
 
-# UPX=false
-# if hash upx 2>/dev/null; then
-# 	UPX=true
-# fi
-
-VERSION=$(curl -sSL https://api.github.com/repos/honwen/aliyun-ddns-cli/commits/master | sed -n '{/sha/p; /date/p;}'| sed 's/.* \"//g' | cut -c1-10 | tr '[:lower:]' '[:upper:]' | sed 'N;s/\n/@/g' | head -1)
+VERSION=$(curl -sSL https://api.github.com/repos/Droid-MAX/aliyun-ddns-cli/commits/master | sed -n '{/sha/p; /date/p;}'| sed 's/.* \"//g' | cut -c1-10 | tr '[:lower:]' '[:upper:]' | sed 'N;s/\n/@/g' | head -1)
 LDFLAGS="-X main.version=$VERSION -s -w"
 
 # X86
@@ -28,32 +23,33 @@ for os in ${OSES[@]}; do
 			suffix=".exe"
 		fi
 		env CGO_ENABLED=0 GOOS=$os GOARCH=$arch go build -ldflags "$LDFLAGS" -o ./release/${name}_${os}_${arch}${suffix} .
-		# if $UPX; then upx -9 ./release/${name}_${os}_${arch}${suffix} -o ./release/${name}_${os}_${arch}${suffix}_upx; fi
 		tar -C ./release -zcf ./release/${name}_${os}-${arch}-$VERSION.tar.gz ./${name}_${os}_${arch}${suffix}
 		$MD5 ./release/${name}_${os}-${arch}-$VERSION.tar.gz
 	done
 done
 
-# ARM64
-env CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags "$LDFLAGS" -o ./release/${name}_arm64 .
 # ARM
 ARMS=(5 6 7)
 for v in ${ARMS[@]}; do
 	env CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=$v go build -ldflags "$LDFLAGS" -o ./release/${name}_arm$v .
 done
-# if $UPX; then upx -9 ./release/${name}_arm*; fi
-tar -C ./release -zcf ./release/${name}_arm-$VERSION.tar.gz $(for v in ${ARMS[@]}; do echo -n "./${name}_arm$v ";done)
+# ARM64
+env CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags "$LDFLAGS" -o ./release/${name}_arm64 .
+tar -C ./release -zcf ./release/${name}_arm-$VERSION.tar.gz $(for v in ${ARMS[@]}; do echo -n "./${name}_arm$v ";done) ${name}_arm64
 $MD5 ./release/${name}_arm-$VERSION.tar.gz
 
 # MIPS/hardfloat
 env CGO_ENABLED=0 GOOS=linux GOARCH=mipsle go build -ldflags "$LDFLAGS" -o ./release/${name}_mipsle .
+tar -C ./release -zcf ./release/${name}_mipsle-$VERSION.tar.gz ./${name}_mipsle
+$MD5 ./release/${name}_mipsle-$VERSION.tar.gz
 env CGO_ENABLED=0 GOOS=linux GOARCH=mips go build -ldflags "$LDFLAGS" -o ./release/${name}_mips .
+tar -C ./release -zcf ./release/${name}_mips-$VERSION.tar.gz ./${name}_mips
+$MD5 ./release/${name}_mips-$VERSION.tar.gz
+
 # MIPS/softfloat
 env CGO_ENABLED=0 GOOS=linux GOARCH=mipsle GOMIPS=softfloat go build -ldflags "$LDFLAGS" -o ./release/${name}_mipsle_sf .
-env CGO_ENABLED=0 GOOS=linux GOARCH=mips GOMIPS=softfloat go build -ldflags "$LDFLAGS" -o ./release/${name}_mips_sf .
-# if $UPX; then upx -9 ./release/${name}_mips**; fi
-tar -C ./release -zcf ./release/${name}_mipsle-$VERSION.tar.gz ./${name}_mipsle
-tar -C ./release -zcf ./release/${name}_mips-$VERSION.tar.gz ./${name}_mips
 tar -C ./release -zcf ./release/${name}_mipsle-sf-$VERSION.tar.gz ./${name}_mipsle_sf
+$MD5 ./release/${name}_mipsle-sf-$VERSION.tar.gz
+env CGO_ENABLED=0 GOOS=linux GOARCH=mips GOMIPS=softfloat go build -ldflags "$LDFLAGS" -o ./release/${name}_mips_sf .
 tar -C ./release -zcf ./release/${name}_mips-sf-$VERSION.tar.gz ./${name}_mips_sf
-$MD5 ./release/${name}_mipsle-$VERSION.tar.gz
+$MD5 ./release/${name}_mips-sf-$VERSION.tar.gz
